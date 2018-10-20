5974f8b0cdfaf5bc6ba592508619f2c3b853fff4
mammix2 <mammix2@hotmail.com>
2018-10-20 23:22:37 +0100

    GUI - Zcoin removal

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -210,8 +210,6 @@ private slots:
     void gotoMasternodePage();
     /** Switch to privacy page */
     void gotoReceiveCoinsPage();
-    /** Switch to receive coins page */
-    void gotoPrivacyPage();
     /** Switch to send coins page */
     void gotoSendCoinsPage(QString addr = "");
 
