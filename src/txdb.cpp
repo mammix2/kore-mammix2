140e38d0210202315ec8b2d4cf039924c76c689d
Lico <lico@coreblockchain.com.br>
2018-11-14 17:05:14 -0200

    adding obfuscation to leveldb and txdb

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -33,14 +33,14 @@ static const char DB_LAST_BLOCK = 'l';
 void static BatchWriteCoins(CLevelDBBatch& batch, const uint256& hash, const CCoins& coins)
 {
     if (coins.IsPruned())
-        batch.Erase(make_pair('c', hash));
+        batch.Erase(make_pair(DB_COINS, hash));
     else
-        batch.Write(make_pair('c', hash), coins);
+        batch.Write(make_pair(DB_COINS, hash), coins);
 }
 
 void static BatchWriteHashBestChain(CLevelDBBatch& batch, const uint256& hash)
 {
-    batch.Write('B', hash);
+    batch.Write(DB_BEST_BLOCK, hash);
 }
 
 CCoinsViewDB::CCoinsViewDB(size_t nCacheSize, bool fMemory, bool fWipe) : db(GetDataDir() / "chainstate", nCacheSize, fMemory, fWipe)
@@ -49,25 +49,25 @@ CCoinsViewDB::CCoinsViewDB(size_t nCacheSize, bool fMemory, bool fWipe) : db(Get
 
 bool CCoinsViewDB::GetCoins(const uint256& txid, CCoins& coins) const
 {
-    return db.Read(make_pair('c', txid), coins);
+    return db.Read(make_pair(DB_COINS, txid), coins);
 }
 
 bool CCoinsViewDB::HaveCoins(const uint256& txid) const
 {
-    return db.Exists(make_pair('c', txid));
+    return db.Exists(make_pair(DB_COINS, txid));
 }
 
 uint256 CCoinsViewDB::GetBestBlock() const
 {
     uint256 hashBestChain;
-    if (!db.Read('B', hashBestChain))
+    if (!db.Read(DB_BEST_BLOCK, hashBestChain))
         return uint256(0);
     return hashBestChain;
 }
 
 bool CCoinsViewDB::BatchWrite(CCoinsMap& mapCoins, const uint256& hashBlock)
 {
-    CLevelDBBatch batch;
+    CLevelDBBatch batch(&db.GetObfuscateKey());
     size_t count = 0;
     size_t changed = 0;
     for (CCoinsMap::iterator it = mapCoins.begin(); it != mapCoins.end();) {
@@ -92,41 +92,41 @@ CBlockTreeDB::CBlockTreeDB(size_t nCacheSize, bool fMemory, bool fWipe) : CLevel
 
 bool CBlockTreeDB::WriteBlockIndex(const CDiskBlockIndex& blockindex)
 {
-    return Write(make_pair('b', blockindex.GetBlockHash()), blockindex);
+    return Write(make_pair(DB_BLOCK_INDEX, blockindex.GetBlockHash()), blockindex);
 }
 
 bool CBlockTreeDB::WriteBlockFileInfo(int nFile, const CBlockFileInfo& info)
 {
-    return Write(make_pair('f', nFile), info);
+    return Write(make_pair(DB_BLOCK_FILES, nFile), info);
 }
 
 bool CBlockTreeDB::ReadBlockFileInfo(int nFile, CBlockFileInfo& info)
 {
-    return Read(make_pair('f', nFile), info);
+    return Read(make_pair(DB_BLOCK_FILES, nFile), info);
 }
 
 bool CBlockTreeDB::WriteLastBlockFile(int nFile)
 {
-    return Write('l', nFile);
+    return Write(DB_LAST_BLOCK, nFile);
 }
 
 bool CBlockTreeDB::WriteReindexing(bool fReindexing)
 {
     if (fReindexing)
-        return Write('R', '1');
+        return Write(DB_REINDEX_FLAG, '1');
     else
-        return Erase('R');
+        return Erase(DB_REINDEX_FLAG);
 }
 
 bool CBlockTreeDB::ReadReindexing(bool& fReindexing)
 {
-    fReindexing = Exists('R');
+    fReindexing = Exists(DB_REINDEX_FLAG);
     return true;
 }
 
 bool CBlockTreeDB::ReadLastBlockFile(int& nFile)
 {
-    return Read('l', nFile);
+    return Read(DB_LAST_BLOCK, nFile);
 }
 
 bool CCoinsViewDB::GetStats(CCoinsStats& stats) const
@@ -134,7 +134,7 @@ bool CCoinsViewDB::GetStats(CCoinsStats& stats) const
     /* It seems that there are no "const iterators" for LevelDB.  Since we
        only need read operations on it, use a const-cast to get around
        that restriction.  */
-    boost::scoped_ptr<leveldb::Iterator> pcursor(const_cast<CLevelDBWrapper*>(&db)->NewIterator());
+    boost::scoped_ptr<CLevelDBIterator> pcursor(const_cast<CLevelDBWrapper*>(&db)->NewIterator());
     pcursor->SeekToFirst();
 
     CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
@@ -144,83 +144,75 @@ bool CCoinsViewDB::GetStats(CCoinsStats& stats) const
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         try {
-            leveldb::Slice slKey = pcursor->key();
-            CDataStream ssKey(slKey.data(), slKey.data() + slKey.size(), SER_DISK, CLIENT_VERSION);
-            char chType;
-            ssKey >> chType;
-            if (chType == 'c') {
-                leveldb::Slice slValue = pcursor->value();
-                CDataStream ssValue(slValue.data(), slValue.data() + slValue.size(), SER_DISK, CLIENT_VERSION);
-                CCoins coins;
-                ssValue >> coins;
-                uint256 txhash;
-                ssKey >> txhash;
-                ss << txhash;
-                ss << VARINT(coins.nVersion);
-                ss << (coins.fCoinBase ? 'c' : 'n');
-                ss << VARINT(coins.nHeight);
-                stats.nTransactions++;
-                for (unsigned int i=0; i<coins.vout.size(); i++) {
-                    const CTxOut &out = coins.vout[i];
-                    if (!out.IsNull()) {
-                        stats.nTransactionOutputs++;
-                        ss << VARINT(i+1);
-                        ss << out;
-                        nTotalAmount += out.nValue;
+            std::pair<char, uint256> key;
+            CCoins coins;
+            if (pcursor->GetKey(key) && key.first == DB_COINS) {
+                if (pcursor->GetValue(coins)) {
+                    stats.nTransactions++;
+                    for (unsigned int i = 0; i < coins.vout.size(); i++) {
+                        const CTxOut& out = coins.vout[i];
+                        if (!out.IsNull()) {
+                            stats.nTransactionOutputs++;
+                            ss << VARINT(i + 1);
+                            ss << out;
+                            nTotalAmount += out.nValue;
+                        }
                     }
+                    stats.nSerializedSize += 32 + pcursor->GetValueSize();
+                    ss << VARINT(0);
+                } else {
+                    return error("CCoinsViewDB::GetStats() : unable to read value");
                 }
-                stats.nSerializedSize += 32 + slValue.size();
-                ss << VARINT(0);
             }
             pcursor->Next();
         } catch (std::exception& e) {
             return error("%s : Deserialize or I/O error - %s", __func__, e.what());
         }
     }
-    stats.nHeight = mapBlockIndex.find(GetBestBlock())->second->nHeight;
+    {
+        LOCK(cs_main);
+        stats.nHeight = mapBlockIndex.find(stats.hashBlock)->second->nHeight;
+    }
     stats.hashSerialized = ss.GetHash();
     stats.nTotalAmount = nTotalAmount;
     return true;
 }
 
-bool CBlockTreeDB::WriteBatchSync(const std::vector<std::pair<int, const CBlockFileInfo*> >& fileInfo, int nLastFile, const std::vector<const CBlockIndex*>& blockinfo) {
-    
-    //CDBBatch batch(&GetObfuscateKey());
-    CLevelDBBatch batch;
-    for (std::vector<std::pair<int, const CBlockFileInfo*> >::const_iterator it=fileInfo.begin(); it != fileInfo.end(); it++) {
+bool CBlockTreeDB::WriteBatchSync(const std::vector<std::pair<int, const CBlockFileInfo*> >& fileInfo, int nLastFile, const std::vector<const CBlockIndex*>& blockinfo)
+{
+    CLevelDBBatch batch(&GetObfuscateKey());
+    for (std::vector<std::pair<int, const CBlockFileInfo*> >::const_iterator it = fileInfo.begin(); it != fileInfo.end(); it++) {
         batch.Write(make_pair(DB_BLOCK_FILES, it->first), *it->second);
     }
     batch.Write(DB_LAST_BLOCK, nLastFile);
-    for (std::vector<const CBlockIndex*>::const_iterator it=blockinfo.begin(); it != blockinfo.end(); it++) {
+    for (std::vector<const CBlockIndex*>::const_iterator it = blockinfo.begin(); it != blockinfo.end(); it++) {
         batch.Write(make_pair(DB_BLOCK_INDEX, (*it)->GetBlockHash()), CDiskBlockIndex(*it));
     }
-
     return WriteBatch(batch, true);
-    
 }
 
 bool CBlockTreeDB::ReadTxIndex(const uint256& txid, CDiskTxPos& pos)
 {
-    return Read(make_pair('t', txid), pos);
+    return Read(make_pair(DB_TXINDEX, txid), pos);
 }
 
 bool CBlockTreeDB::WriteTxIndex(const std::vector<std::pair<uint256, CDiskTxPos> >& vect)
 {
-    CLevelDBBatch batch;
+    CLevelDBBatch batch(&GetObfuscateKey());
     for (std::vector<std::pair<uint256, CDiskTxPos> >::const_iterator it = vect.begin(); it != vect.end(); it++)
-        batch.Write(make_pair('t', it->first), it->second);
+        batch.Write(make_pair(DB_TXINDEX, it->first), it->second);
     return WriteBatch(batch);
 }
 
 bool CBlockTreeDB::WriteFlag(const std::string& name, bool fValue)
 {
-    return Write(std::make_pair('F', name), fValue ? '1' : '0');
+    return Write(std::make_pair(DB_FLAG, name), fValue ? '1' : '0');
 }
 
 bool CBlockTreeDB::ReadFlag(const std::string& name, bool& fValue)
 {
     char ch;
-    if (!Read(std::make_pair('F', name), ch))
+    if (!Read(std::make_pair(DB_FLAG, name), ch))
         return false;
     fValue = ch == '1';
     return true;
@@ -239,63 +231,58 @@ bool CBlockTreeDB::ReadInt(const std::string& name, int& nValue)
 bool CBlockTreeDB::LoadBlockIndexGuts()
 {
     LogPrintf("LoadBlockIndexGuts --> \n");
-    boost::scoped_ptr<leveldb::Iterator> pcursor(NewIterator());
+    boost::scoped_ptr<CLevelDBIterator> pcursor(NewIterator());
 
-    CDataStream ssKeySet(SER_DISK, CLIENT_VERSION);
-    ssKeySet << make_pair('b', uint256(0));
-    pcursor->Seek(ssKeySet.str());
+    pcursor->Seek(make_pair(DB_BLOCK_INDEX, uint256()));
 
     // Load mapBlockIndex
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         try {
-            leveldb::Slice slKey = pcursor->key();
-            CDataStream ssKey(slKey.data(), slKey.data() + slKey.size(), SER_DISK, CLIENT_VERSION);
-            char chType;
-            ssKey >> chType;
-            if (chType == 'b') {
-                leveldb::Slice slValue = pcursor->value();
-                CDataStream ssValue(slValue.data(), slValue.data() + slValue.size(), SER_DISK, CLIENT_VERSION);
+            std::pair<char, uint256> key;
+            if (pcursor->GetKey(key) && key.first == DB_BLOCK_INDEX) {
                 CDiskBlockIndex diskindex;
-                ssValue >> diskindex;
+                if (pcursor->GetValue(diskindex)) {
+                    // Construct block index object
+                    CBlockIndex* pindexNew = InsertBlockIndex(diskindex.GetBlockHash());
+                    pindexNew->pprev = InsertBlockIndex(diskindex.hashPrev);
+                    pindexNew->pnext = InsertBlockIndex(diskindex.hashNext);
+                    pindexNew->nHeight = diskindex.nHeight;
+                    pindexNew->nFile = diskindex.nFile;
+                    pindexNew->nDataPos = diskindex.nDataPos;
+                    pindexNew->nUndoPos = diskindex.nUndoPos;
+                    pindexNew->nVersion = diskindex.nVersion;
+                    pindexNew->hashMerkleRoot = diskindex.hashMerkleRoot;
+                    pindexNew->nTime = diskindex.nTime;
+                    pindexNew->nBits = diskindex.nBits;
+                    pindexNew->nNonce = diskindex.nNonce;
+                    pindexNew->nBirthdayA = diskindex.nBirthdayA;
+                    pindexNew->nBirthdayB = diskindex.nBirthdayB;
+                    pindexNew->nStatus = diskindex.nStatus;
+                    pindexNew->nTx = diskindex.nTx;
 
-                // Construct block index object
-                CBlockIndex* pindexNew = InsertBlockIndex(diskindex.GetBlockHash());
-                pindexNew->pprev = InsertBlockIndex(diskindex.hashPrev);
-                pindexNew->pnext = InsertBlockIndex(diskindex.hashNext);
-                pindexNew->nHeight = diskindex.nHeight;
-                pindexNew->nFile = diskindex.nFile;
-                pindexNew->nDataPos = diskindex.nDataPos;
-                pindexNew->nUndoPos = diskindex.nUndoPos;
-                pindexNew->nVersion = diskindex.nVersion;
-                pindexNew->hashMerkleRoot = diskindex.hashMerkleRoot;
-                pindexNew->nTime = diskindex.nTime;
-                pindexNew->nBits = diskindex.nBits;
-                pindexNew->nNonce = diskindex.nNonce;
-                pindexNew->nBirthdayA     = diskindex.nBirthdayA;
-                pindexNew->nBirthdayB     = diskindex.nBirthdayB;
-                pindexNew->nStatus = diskindex.nStatus;
-                pindexNew->nTx = diskindex.nTx;
+                    //Proof Of Stake
+                    pindexNew->nMint = diskindex.nMint;
+                    pindexNew->nMoneySupply = diskindex.nMoneySupply;
+                    pindexNew->nFlags = diskindex.nFlags;
+                    pindexNew->nStakeModifier = diskindex.nStakeModifier;
+                    pindexNew->nStakeModifierOld = diskindex.nStakeModifierOld;
+                    pindexNew->prevoutStake = diskindex.prevoutStake;
+                    pindexNew->nStakeTime = diskindex.nStakeTime;
+                    pindexNew->hashProofOfStake = diskindex.hashProofOfStake;
 
-                //Proof Of Stake
-                pindexNew->nMint = diskindex.nMint;
-                pindexNew->nMoneySupply = diskindex.nMoneySupply;
-                pindexNew->nFlags = diskindex.nFlags;
-                pindexNew->nStakeModifier = diskindex.nStakeModifier;
-                pindexNew->nStakeModifierOld = diskindex.nStakeModifierOld;
-                pindexNew->prevoutStake = diskindex.prevoutStake;
-                pindexNew->nStakeTime = diskindex.nStakeTime;
-                pindexNew->hashProofOfStake = diskindex.hashProofOfStake;
+                    if (!pindexNew->IsProofOfStake() && (pindexNew->nStatus & BLOCK_HAVE_DATA)) {
+                        if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, pindexNew->nHeight))
+                            return error("LoadBlockIndex() : CheckProofOfWork failed: %s", pindexNew->ToString());
+                    }
+                    // ppcoin: build setStakeSeen
+                    if (pindexNew->IsProofOfStake())
+                        setStakeSeen.insert(make_pair(pindexNew->prevoutStake, pindexNew->nStakeTime));
 
-                if (!pindexNew->IsProofOfStake() && (pindexNew->nStatus & BLOCK_HAVE_DATA)) {
-                    if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits, pindexNew->nHeight))
-                        return error("LoadBlockIndex() : CheckProofOfWork failed: %s", pindexNew->ToString());
+                    pcursor->Next();
+                } else {
+                    return error("LoadBlockIndex() : failed to read value");
                 }
-                // ppcoin: build setStakeSeen
-                if (pindexNew->IsProofOfStake())
-                    setStakeSeen.insert(make_pair(pindexNew->prevoutStake, pindexNew->nStakeTime));
-                
-                pcursor->Next();
             } else {
                 break; // if shutdown requested or finished loading block index
             }
@@ -308,123 +295,3 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
     LogPrintf("LoadBlockIndexGuts <-- \n");
     return true;
 }
-
-#ifdef ZEROCOIN
-CZerocoinDB::CZerocoinDB(size_t nCacheSize, bool fMemory, bool fWipe) : CLevelDBWrapper(GetDataDir() / "zerocoin", nCacheSize, fMemory, fWipe)
-{
-}
-
-bool CZerocoinDB::WriteCoinMint(const PublicCoin& pubCoin, const uint256& hashTx)
-{
-    uint256 hash = GetPubCoinHash(pubCoin.getValue());
-    return Write(make_pair('m', hash), hashTx, true);
-}
-
-bool CZerocoinDB::ReadCoinMint(const CBigNum& bnPubcoin, uint256& hashTx)
-{
-    return ReadCoinMint(GetPubCoinHash(bnPubcoin), hashTx);
-}
-
-bool CZerocoinDB::ReadCoinMint(const uint256& hashPubcoin, uint256& hashTx)
-{
-    return Read(make_pair('m', hashPubcoin), hashTx);
-}
-
-bool CZerocoinDB::EraseCoinMint(const CBigNum& bnPubcoin)
-{
-    uint256 hash = GetPubCoinHash(bnPubcoin);
-    return Erase(make_pair('m', hash));
-}
-
-bool CZerocoinDB::WriteCoinSpend(const CBigNum& bnSerial, const uint256& txHash)
-{
-    CDataStream ss(SER_GETHASH, 0);
-    ss << bnSerial;
-    uint256 hash = Hash(ss.begin(), ss.end());
-
-    return Write(make_pair('s', hash), txHash, true);
-}
-
-bool CZerocoinDB::ReadCoinSpend(const CBigNum& bnSerial, uint256& txHash)
-{
-    CDataStream ss(SER_GETHASH, 0);
-    ss << bnSerial;
-    uint256 hash = Hash(ss.begin(), ss.end());
-
-    return Read(make_pair('s', hash), txHash);
-}
-
-bool CZerocoinDB::ReadCoinSpend(const uint256& hashSerial, uint256 &txHash)
-{
-    return Read(make_pair('s', hashSerial), txHash);
-}
-
-bool CZerocoinDB::EraseCoinSpend(const CBigNum& bnSerial)
-{
-    CDataStream ss(SER_GETHASH, 0);
-    ss << bnSerial;
-    uint256 hash = Hash(ss.begin(), ss.end());
-
-    return Erase(make_pair('s', hash));
-}
-
-bool CZerocoinDB::WipeCoins(std::string strType)
-{
-    if (strType != "spends" && strType != "mints")
-        return error("%s: did not recognize type %s", __func__, strType);
-
-    boost::scoped_ptr<leveldb::Iterator> pcursor(NewIterator());
-
-    char type = (strType == "spends" ? 's' : 'm');
-    CDataStream ssKeySet(SER_DISK, CLIENT_VERSION);
-    ssKeySet << make_pair(type, uint256(0));
-    pcursor->Seek(ssKeySet.str());
-    // Load mapBlockIndex
-    std::set<uint256> setDelete;
-    while (pcursor->Valid()) {
-        boost::this_thread::interruption_point();
-        try {
-            leveldb::Slice slKey = pcursor->key();
-            CDataStream ssKey(slKey.data(), slKey.data() + slKey.size(), SER_DISK, CLIENT_VERSION);
-            char chType;
-            ssKey >> chType;
-            if (chType == type) {
-                leveldb::Slice slValue = pcursor->value();
-                CDataStream ssValue(slValue.data(), slValue.data() + slValue.size(), SER_DISK, CLIENT_VERSION);
-                uint256 hash;
-                ssValue >> hash;
-                setDelete.insert(hash);
-                pcursor->Next();
-            } else {
-                break; // if shutdown requested or finished loading block index
-            }
-        } catch (std::exception& e) {
-            return error("%s : Deserialize or I/O error - %s", __func__, e.what());
-        }
-    }
-
-    for (auto& hash : setDelete) {
-        if (!Erase(make_pair(type, hash)))
-            LogPrintf("%s: error failed to delete %s\n", __func__, hash.GetHex());
-    }
-
-    return true;
-}
-
-bool CZerocoinDB::WriteAccumulatorValue(const uint32_t& nChecksum, const CBigNum& bnValue)
-{
-    LogPrint("zero","%s : checksum:%d val:%s\n", __func__, nChecksum, bnValue.GetHex());
-    return Write(make_pair('2', nChecksum), bnValue);
-}
-
-bool CZerocoinDB::ReadAccumulatorValue(const uint32_t& nChecksum, CBigNum& bnValue)
-{
-    return Read(make_pair('2', nChecksum), bnValue);
-}
-
-bool CZerocoinDB::EraseAccumulatorValue(const uint32_t& nChecksum)
-{
-    LogPrint("zero", "%s : checksum:%d\n", __func__, nChecksum);
-    return Erase(make_pair('2', nChecksum));
-}
-#endif
