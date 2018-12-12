8c003adb2aa43b4e18505f112c5a83920e7e3529
EG <nsg1@protonmail.com>
2018-12-12 05:59:49 -0800

    Fix tests and params

--- src/test/rpc_tests.cpp
+++ src/test/rpc_tests.cpp
@@ -70,7 +70,7 @@ BOOST_AUTO_TEST_CASE(rpc_rawparams)
     BOOST_CHECK_THROW(CallRPC("decoderawtransaction"), runtime_error);
     BOOST_CHECK_THROW(CallRPC("decoderawtransaction null"), runtime_error);
     BOOST_CHECK_THROW(CallRPC("decoderawtransaction DEADBEEF"), runtime_error);
-    string rawtx = "0100000001a15d57094aa7a21a28cb20b59aab8fc7d1149a3bdbcddba9c622e4f5f6a99ece010000006c493046022100f93bb0e7d8db7bd46e40132d1f8242026e045f03a0efe71bbb8e3f475e970d790221009337cd7f1f929f00cc6ff01f03729b069a7c21b59b1736ddfee5db5946c5da8c0121033b9b137ee87d5a812d6f506efdd37f0affa7ffc310711c06c7f3e097c9447c52ffffffff0100e1f505000000001976a9140389035a9225b3839e2bbf32d826a1e222031fd888ac00000000";
+    string rawtx = "010000000000000001bba8163874efcb793ef83800a1bd8c0922fac4d7a75b231d4ee627091a55f1570000000000ffffffff02d00ed717000000001976a914ff197b14e502ab41f3bc8ccb48c4abac9eab35bc88ac00e1f505000000001976a914d818ef7432c0b0805ebea9a0738030dfe821279b88ac00000000";
     BOOST_CHECK_NO_THROW(r = CallRPC(string("decoderawtransaction ")+rawtx));
     BOOST_CHECK_EQUAL(find_value(r.get_obj(), "version").get_int(), 1);
     BOOST_CHECK_EQUAL(find_value(r.get_obj(), "locktime").get_int(), 0);
@@ -98,12 +98,12 @@ BOOST_AUTO_TEST_CASE(rpc_rawsign)
     string prevout =
       "[{\"txid\":\"dd2888870cdc3f6e92661f6b0829667ee4bb07ed086c44205e726bbf3338f726\","
       "\"vout\":1,\"scriptPubKey\":\"a914f5404a39a4799d8710e15db4c4512c5e06f97fed87\","
-      "\"redeemScript\":\"5121021431a18c7039660cd9e3612a2a47dc53b69cb38ea4ad743b7df8245fd0438f8e21029bbeff390ce736bd396af43b52a1c14ed52c086b1e5585c15931f68725772bac52ae\"}]";
+      "\"redeemScript\":\"51210308140196125a03aac4410ade92544362dbc3a6a486edcf207a58d276efdd7a3421026f48ccec6ee60c6ad7ead41812f518eb4b1de813cadbb2bd0c2e343d0cda15f152ae\"}]";
     r = CallRPC(string("createrawtransaction ")+prevout+" "+
-      "{\"6ckcNMWRYgTnPcrTXCdwhDnMLwj3zwseej\":1}");
+      "{\"KJdsLBPJxKNwnnSRSHt45zE6153bYCwekg\":1}");
     string notsigned = r.get_str();
-    string privkey1 = "\"YVobcS47fr6kceZy9LzLJR8WQ6YRpUwYKoJhrnEXepebMxaSpbnn\"";
-    string privkey2 = "\"YRyMjG8hbm8jHeDMAfrzSeHq5GgAj7kuHFvJtMudCUH3sCkq1WtA\"";
+    string privkey1 = "\"L3pWsPEtpK8v5pLB6SxTUTAS2rcqKFoc1Zv1hkQQeEMxaUdb4RDq\"";
+    string privkey2 = "\"L3pzXSSqfyXWrz8YtFGg5MPaTttw8suMUcQdfdutz7ikPkRSti8Z\"";
     r = CallRPC(string("signrawtransaction ")+notsigned+" "+prevout+" "+"[]");
     BOOST_CHECK(find_value(r.get_obj(), "complete").get_bool() == false);
     r = CallRPC(std::string("signrawtransaction ")+notsigned+" "+prevout+" "+"["+privkey1+","+privkey2+"]");
