927742d9f65982ca15607e1ecfe8a04c04e58623
Lico <lico@coreblockchain.com.br>
2019-03-14 17:48:48 -0300

    fixing masternode rewards

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -338,17 +338,16 @@ CAmount GetMasternodePayment(CAmount blockReward, CAmount stakedBalance, CBlockI
 {
     double moneySupplyDouble = pindexPrev->nMoneySupply;
     double blockRewardDouble = (double)blockReward;
-    double stakedBalanceDouble = (double)max(stakedBalance, 5000 * COIN);
+    double stakedBalanceDouble = (double)min(stakedBalance, 5000 * COIN);
 
-    double stakedBalanceSquare = pow(stakedBalance, 2);
+    double stakedBalanceSquare = pow(stakedBalanceDouble, 2);
     double stakedBalanceTimesConstant1 = 8.00011e-13 * stakedBalanceDouble;
     double stakedBalanceTimesConstant2 = 1.17928e-58 * stakedBalanceSquare;
     stakedBalanceTimesConstant2 *= moneySupplyDouble;
-    stakedBalanceDouble = stakedBalanceTimesConstant1 + stakedBalanceTimesConstant2 + 0.95;
+    stakedBalanceDouble = 1 - stakedBalanceTimesConstant1 + stakedBalanceTimesConstant2 + 0.05;
 
     return blockRewardDouble * stakedBalanceDouble;
 }
-
 
 void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int64_t nFees, bool fProofOfStake, CAmount nstakedBallance)
 {
